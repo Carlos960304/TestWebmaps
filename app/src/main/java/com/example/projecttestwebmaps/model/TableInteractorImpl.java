8e3756bfa24482b1f42cc503bba1c20d094d93ec
Carlos960304 <carlos.delcarmen@hotmail.com>
2021-07-08 22:28:57 -0500

    Creacion del patron MVP

---
+++ app/src/main/java/com/example/projecttestwebmaps/model/TableInteractorImpl.java
@@ -0,0 +1,4 @@
+package com.example.projecttestwebmaps.model;
+
+public class TableInteractorImpl {
+}
---
+++ app/src/main/java/com/example/projecttestwebmaps/presenter/TablePresenterImpl.java
@@ -0,0 +1,4 @@
+package com.example.projecttestwebmaps.presenter;
+
+public class TablePresenterImpl {
+}
